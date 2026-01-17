0c05acc36608a7c4776bcf6f8e9b0eb1474828a7
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-01-17 18:22:39 +0530

    Create 12_TypeCasting.cpp

---
+++ UNIT-1_BASICS_OF_OOP_CPP/12_TypeCasting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 3;
+    float result;
+
+    result = (float)a / b;   // Typecasting int to float
+
+    cout << "Result after typecasting = " << result << endl;
+
+    return 0;
+}
